70ac0198f118cc06044555c24cde9874177c1a60
Bara1234 <phfbara@gmail.com>
2026-04-13 11:29:36 -0300

    Metodo tabuleiro

---
+++ xadrez/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,21 @@
+﻿namespace tabuleiro {
+    class Tabuleiro {
+
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+        private Peca[,] pecas;
+
+        public Tabuleiro(int linha, int coluna) {
+            this.linhas = linha;
+            this.colunas = coluna;
+            pecas = new Peca[linhas, colunas];
+        }
+
+        public Peca peca(int linha, int coluna) {
+            return pecas[linha, coluna];
+        }
+    }
+}
+
+
+
